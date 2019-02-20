2da7b1e96b27b0ddb3e51439a785fa1eb6ccd33e
v7lin <v7lin@qq.com>
2019-02-20 17:15:40 +0800

    初始化

---
+++ app/src/main/java/io/github/v7lin/oem/push/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.v7lin.oem.push;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
